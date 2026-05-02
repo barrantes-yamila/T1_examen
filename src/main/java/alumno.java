84b86acbddf10fc174c8f851fdbe57c38867ed55
barrantes-yamila <barrantesyamila@gmail.com>
2026-05-02 13:35:52 -0500

    nomesaleayuda

--- src/main/java/alumno.java
+++ src/main/java/alumno.java
@@ -8,5 +8,92 @@
  * @author Yamila
  */
 public class alumno {
+ public class Alumno {
+    private String nombre;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private char nivelSocioeconomico;
+    private String tipoBeca;
+    private float porcentajeBeca;
+    private float pensionBase;
+    private float montoPensionFinal;
+
+    public Alumno(String nombre, String tipoDocumento, String numeroDocumento, char nivelSocioeconomico) {
+        this.nombre = nombre;
+        this.tipoDocumento = tipoDocumento;
+        this.nivelSocioeconomico = nivelSocioeconomico;
+        this.setNumeroDocumento(tipoDocumento, numeroDocumento);
+        this.pensionBase = asignarPensionBase(nivelSocioeconomico);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+    public boolean setNumeroDocumento(String tipo, String numero) {
+        if (tipo.equals("DNI") && numero.length() == 8) {
+            this.numeroDocumento = numero;
+            return true;
+        } else if (tipo.equals("CE") && numero.length() == 11) {
+            this.numeroDocumento = numero;
+            return true;
+        } else {
+            System.out.println("Error: documento invalido para el tipo " + tipo);
+            return false;
+        }
+    }
+
+    public char getNivelSocioeconomico() {
+        return nivelSocioeconomico;
+    }
+    public void setNivelSocioeconomico(char nivel) {
+        this.nivelSocioeconomico = nivel;
+        this.pensionBase = asignarPensionBase(nivel);
+    }
+
+    public void setPorcentajeBeca(String tipoBeca, float porcentaje) {
+        this.tipoBeca = tipoBeca;
+        this.porcentajeBeca = porcentaje;
+    }
+
+    private float asignarPensionBase(char nivel) {
+        switch (nivel) {
+            case 'A': return 500.0f;
+            case 'B': return 350.0f;
+            case 'C': return 200.0f;
+            default:
+                System.out.println("Nivel invalido, se asigna pension base 0.");
+                return 0.0f;
+        }
+    }
+
+    public float calcularPensionFinal() {
+        float descuento = this.pensionBase * (this.porcentajeBeca / 100);
+        this.montoPensionFinal = this.pensionBase - descuento;
+        return this.montoPensionFinal;
+    }
+
+    public void verDatos() {
+        System.out.println("Alumno");
+        System.out.println("Nombre            : " + this.nombre);
+        System.out.println("Tipo documento    : " + this.tipoDocumento);
+        System.out.println("Numero documento  : " + this.numeroDocumento);
+        System.out.println("Nivel socioeconomico: " + this.nivelSocioeconomico);
+        System.out.println("Tipo de beca      : " + this.tipoBeca);
+        System.out.println("Porcentaje beca   : " + this.porcentajeBeca + "%");
+        System.out.println("Pension base      : S/ " + this.pensionBase);
+        System.out.println("Pension final     : S/ " + this.calcularPensionFinal());
+    }
+}
     
 }
