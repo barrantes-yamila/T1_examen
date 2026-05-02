fa1ef10671ef4bf3bf3ca2cbbdecf117c7150eb9
barrantes-yamila <barrantesyamila@gmail.com>
2026-05-02 13:25:40 -0500

    primerintento

---
+++ src/main/java/com/mycompany/sistemapoot1/Sistemapoot1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemapoot1;
+
+/**
+ *
+ * @author Yamila
+ */
+public class Sistemapoot1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
